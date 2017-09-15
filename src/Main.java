9bb88e9545d829528c165a54814ed1ae1dc1eaf7
SURYARAJYALAKSHMI <31991076+SURYARAJYALAKSHMI@users.noreply.github.com>
2017-09-15 23:31:18 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	Student c=new Student(12,ram,1997-5-12,55);
+	Student c1=new Student(13,rahim,1993-5-1,40);
+	Student c2=new Student(14,sita,1997-10-2,85);
+	Student c3=new Student(15,krishna,2000-9-12,35);
+	
+	
+	}
+
+}
